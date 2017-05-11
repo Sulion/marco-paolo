60480f43b41225506edc6a0ab0b4d7dadc3a313e
Sulion <michael.rykov@gmail.com>
2017-05-12 00:35:31 +0300

    Extend the model to accomodate for descriptions

--- src/main/kotlin/org/logout/notifications/telegram/data/events/Event.kt
+++ src/main/kotlin/org/logout/notifications/telegram/data/events/Event.kt
@@ -5,7 +5,8 @@ import java.util.*
 data class Event(val startDate: Date,
                  val eventName: String,
                  val performerName: String,
-                 val trackName: String) {
+                 val trackName: String,
+                 val eventDescription: String? = null) {
     constructor() : this(Date(), "", "", "")
 }
 
