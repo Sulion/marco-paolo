bb23b8bf8e588edc116180dd470b38160a5246d7
Sulion <michael.rykov@gmail.com>
2017-05-12 18:51:46 +0300

    Greet yourself

--- src/main/kotlin/org/logout/Application.kt
+++ src/main/kotlin/org/logout/Application.kt
@@ -1,10 +1,14 @@
 package org.logout
 
 import org.logout.notifications.telegram.infobipbot.InfobipTelegramBot
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.scheduling.TaskScheduler
+import org.springframework.web.client.RestTemplate
+import javax.annotation.PostConstruct
 
 
 @SpringBootApplication
@@ -13,11 +17,36 @@ open class Application {
     @Autowired
     lateinit var bot: InfobipTelegramBot
 
+    @Autowired
+    lateinit var restTemplate: RestTemplate
+
+    @Autowired
+    lateinit var taskExecutor: TaskScheduler
+
+    @PostConstruct
+    open fun pingMyself() {
+        taskExecutor.scheduleWithFixedDelay(
+                {
+                    try {
+                        val greeting = restTemplate.getForObject(
+                                "https://marco-paolo.herokuapp.com/greeting?name=myself",
+                                String::class.java)
+                        log.info("I have received a greeting from myself: {}", greeting)
+                    } catch (ex: Exception) {
+                        log.info(ex.message, ex)
+                    }
+                },
+                20 * 60 * 1000)
+    }
+
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             SpringApplicationBuilder(Application::class.java).run(*args)
         }
+
+        val log = LoggerFactory.getLogger(Application::class.java)
     }
 }
 
